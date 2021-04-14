d3987c9a135b4d453412cf9050dd030fcb71e7f9
Prem0307 <76106328+Prem0307@users.noreply.github.com>
2021-04-14 12:36:34 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :1.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+      this.image = loadImage("block.png")
+      this.visibility = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      if(this.body.speed<3){
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0, this.width, this.height)
+        pop ();
+      }else{
+        World.remove(world,this.body)
+        push ();
+        this.Visibiltiy = this.Visibilty-5;
+        tint (255,this.Visibiltiy)
+        
+        pop ();
+      }
+    }
+  
+  };
